fd1d3e29d779c96c26c2aed4bbdb83ea902f6e03
Deepika2527 <deepikatrainings.com@gmail.com>
2026-03-12 17:54:11 +0530

    Day3Componets

---
+++ Day3Components/my-comp/src/App.jsx
@@ -0,0 +1,14 @@
+function App(){
+  return (
+    <>
+  <h1>Hello</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, facilis provident! Labore cum asperiores hic voluptatem facere beatae exercitationem fuga a debitis esse. Adipisci, modi quisquam autem tempora quae sed.</p>
+  <h2>Hello all <span>To React classes...</span></h2>
+    </>
+  )
+    
+    
+  
+}
+
+export default App
